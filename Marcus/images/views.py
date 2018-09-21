bab2df6d9a891ff33feda9527e8d9db30218de4c
Jogeonsang <rhcn258@gmail.com>
2018-09-21 13:06:21 +0900

    Like/UnLike, Follow/UnFollow

--- Marcus/images/views.py
+++ Marcus/images/views.py
@@ -36,7 +36,37 @@ class Feed(APIView):
 
 class LikeImage(APIView):
 
-    def get(self, request, image_id, format=None):
+    def post(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NO_FOUND)
+
+        try:
+            preexisiting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
+
+        except models.Like.DoesNotExist:
+        
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image
+            )
+
+            new_like.save()
+            
+            return Response(status=status.HTTP_201_CREATED)
+    
+
+class UnLikeImage(APIView):
+
+    def delete(self, request, image_id, format=None):
 
         user = request.user
 
@@ -51,21 +81,10 @@ class LikeImage(APIView):
                 image=found_image
             )
             preexisiting_like.delete()
-
             return Response(status=status.HTTP_204_NO_CONTENT)
-
         except models.Like.DoesNotExist:
         
-            new_like = models.Like.objects.create(
-                creator=user,
-                image=found_image
-            )
-
-            new_like.save()
-            
-            return Response(status=status.HTTP_201_CREATED)
-    
-
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
 class CommentOnImage(APIView):
 
     def post(self, request, image_id, format=None):
