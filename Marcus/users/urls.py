f2af4c093804f0f2286659d6288b72dd7dc2b19d
Jogeonsang <rhcn258@gmail.com>
2018-09-24 17:58:52 +0900

    Back-UserFollowers,Following List

--- Marcus/users/urls.py
+++ Marcus/users/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("<int:user_id>/follow/", view=views.FollowUser.as_view(), name="follow_users"),
     path("<int:user_id>/unfollow/", view=views.UnFollowUser.as_view(), name="unfollow_users"),
     path("<slug:username>/", view=views.UserProfile.as_view(), name="user_profile"),
+    path("<slug:username>/followers/", view=views.UserFollowers.as_view(), name="user_followers"),
+    path("<slug:username>/following/", view=views.UserFollowing.as_view(), name="user_following"),
 ]
