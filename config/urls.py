672cadda35e53451e47833b095ca6a8de67ac5ad
Jogeonsang <rhcn258@gmail.com>
2018-10-14 23:46:40 +0900

    Backend 와 Frontend 연결
    미리 작업한 Backend와 Frontend를 연결하는 작어블 했습니다.

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 from rest_framework_jwt.views import obtain_jwt_token
+from Marcus import views
 
 urlpatterns = [
     path("rest-auth/", include('rest_auth.urls')),
@@ -26,6 +27,7 @@ urlpatterns = [
     ),
 
     path("accounts/", include("allauth.urls")),
+    path("", views.ReactAppView.as_view()),
     # Your stuff: custom urls includes go here
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
