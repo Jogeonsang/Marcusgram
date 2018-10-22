6ec8f42181dafd904e9b780551f1a64620d4eba5
Jogeonsang <rhcn258@gmail.com>
2018-10-23 00:06:52 +0900

    nav icon을 클릭할때마다 페이지 이동
    index.js에서 pathname을 통해 router가 인식을 해주게 작업했습니다.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,11 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import store, { history } from 'redux/configureStore';
 import App from 'components/App';
-import I18n from 'redux-i18n';
-import { translations } from 'translations';
 
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-        <I18n translations={translations} initialLang="en" fallbackLang="en">
             <App />
-        </I18n>
         </ConnectedRouter>
     </Provider>,
  document.getElementById('root'));
