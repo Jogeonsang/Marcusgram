04420657a1de66910df5ed69e4ab7604d4324d88
Jogeonsang <rhcn258@gmail.com>
2018-10-23 22:24:13 +0900

    loading 컴포넌트 생성 및 스타일링

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -14,7 +14,7 @@ const App = props => [
 
 const PrviateRoutes = props => (
     <Switch>
-        <Route exact path='/' componen={Feed} />
+        <Route exact path='/' component={Feed} />
         <Route exact path='/explore' render={() => 'explore'} />
     </Switch>
 );
--- frontend/src/components/Feed/presenter.js
+++ frontend/src/components/Feed/presenter.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import styles from './styles.scss';
+import React from "react";
+import styles from "./styles.scss";
+import Loading from "components/Loading";
 
-const Feed = props => "Feed!";
+const Feed = props => {
+  if (props.loading) {
+    return <LoadingFeed />;
+  }
+};
+
+const LoadingFeed = props => (
+  <div className={styles.feed}>
+    <Loading />
+  </div>
+);
+
 
 export default Feed;
