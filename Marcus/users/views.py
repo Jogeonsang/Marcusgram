3d1d03bc9aa943a44e93d6bb7c99b9419c0c9e2b
Jogeonsang <rhcn258@gmail.com>
2018-10-23 22:52:45 +0900

    Logout 기능 구현
    Feed 페이지를 구현하면서
    token값이 있을 경우에만 해당 페이지에 접속이 가능하게 설계했기 때문에 로그아웃 기능을 구현했습니다.

--- Marcus/users/views.py
+++ Marcus/users/views.py
@@ -82,7 +82,7 @@ class UserProfile(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         elif found_user.username != user.username:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
         else:
             serializer = serializers.UserProfileSerializer(found_user, data=request.data, partial=True)
@@ -168,7 +168,7 @@ class ChangePassword(APIView):
             else:
                 return Response(status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -3,13 +3,20 @@
 // actions
 
 const SAVE_TOKEN = "SAVE_TOKEN";
+const LOGOUT = "LOGOUT";
 // action creators
 
 function saveToken(token) {
     return {
         type: SAVE_TOKEN,
         token
-    }
+    };
+}
+
+function logout() {
+    return {
+        type: LOGOUT,
+    };
 }
 // API actions
 
@@ -91,6 +98,8 @@ function reducer(state = initialState, action){
     switch(action.type) {
         case SAVE_TOKEN:
             return applySetToken(state, action);
+        case LOGOUT:
+            return applyLogout(state, action);
         default:
         return state;
     }
@@ -105,10 +114,18 @@ function applySetToken(state, action) {
         token
     }
 }
+
+function applyLogout(state, action) {
+    localStorage.removeItem('jwt');
+    return {
+        isLoggedIn: false
+    };
+}
 const actionCreators = {
     facebookLogin,
     usernameLogin,
     createAccount,
+    logout,
 }
 
 export { actionCreators };
